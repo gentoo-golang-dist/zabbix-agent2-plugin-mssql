28bddf4770f9ab9fff5bc264024d2ded1270b679
Rudolfs Zvejs <rudolfs.zvejs@zabbix.com>
2023-12-27 13:12:53 +0200

    [ZBXNEXT-6235] fix job.status.get query column names

--- plugin/queries/job.status.get.sql
+++ plugin/queries/job.status.get.sql
@@ -2,12 +2,12 @@ SELECT sj.name AS job_name,
   sj.enabled AS enabled,
   sjs.last_run_outcome AS run_status,
   sjs.last_outcome_message AS last_run_status_message,
-  sjs.last_run_duration/10000*3600 + sjs.last_run_duration/100%100*60 + sjs.last_run_duration%100 AS RunDuration,
+  sjs.last_run_duration/10000*3600 + sjs.last_run_duration/100%100*60 + sjs.last_run_duration%100 AS run_duration,
   CASE sjs.last_run_date
     WHEN 0 THEN NULL
     ELSE msdb.dbo.agent_datetime(sjs.last_run_date,sjs.last_run_time)
-  END AS last_run_datetime,
-  sja.next_scheduled_run_date AS next_run_datetime
+  END AS last_run_date_time,
+  sja.next_scheduled_run_date AS next_run_date_time
 FROM msdb..sysjobs AS sj
 LEFT JOIN msdb..sysjobservers AS sjs
   ON sj.job_id = sjs.job_id
