9a44792c69204a41b15669ff3e5fdb87b41fe08e
Rudolfs Zvejs <rudolfs.zvejs@zabbix.com>
2024-01-30 13:30:31 +0200

    ...G...... [ZBXNEXT-6235] make job.status.get query return timestamp
    (cherry picked from commit 86951399c1ae86896116a44deeeea7656c02ee95)

--- plugin/queries/job.status.get.sql
+++ plugin/queries/job.status.get.sql
@@ -5,7 +5,10 @@ SELECT sj.name AS job_name,
   sjs.last_run_duration/10000*3600 + sjs.last_run_duration/100%100*60 + sjs.last_run_duration%100 AS run_duration,
   CASE sjs.last_run_date
     WHEN 0 THEN NULL
-    ELSE msdb.dbo.agent_datetime(sjs.last_run_date,sjs.last_run_time)
+    ELSE CONVERT(
+        VARCHAR(25),
+        TODATETIMEOFFSET(msdb.dbo.agent_datetime(sjs.last_run_date,sjs.last_run_time), DATEPART(TZoffset, SYSDATETIMEOFFSET())),
+        126)
   END AS last_run_date_time,
   sja.next_scheduled_run_date AS next_run_date_time
 FROM msdb..sysjobs AS sj
@@ -14,7 +17,10 @@ LEFT JOIN msdb..sysjobservers AS sjs
 LEFT JOIN (
   SELECT job.job_id,
     max(act.session_id) AS s_id,
-    max(act.next_scheduled_run_date) AS next_scheduled_run_date
+    CONVERT(
+        VARCHAR(25),
+        TODATETIMEOFFSET(max(act.next_scheduled_run_date), DATEPART(TZoffset, SYSDATETIMEOFFSET())),
+        126) AS next_scheduled_run_date
   FROM msdb..sysjobs AS job
   LEFT JOIN msdb..sysjobactivity AS act
     ON act.job_id = job.job_id
