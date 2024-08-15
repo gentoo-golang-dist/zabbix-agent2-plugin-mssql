20661d0490b63fd4d4824384f679d5fc3962e810
Eriks Sneiders <eriks.sneiders@zabbix.com>
2024-08-15 11:53:43 +0300

    ...G...... [DEV-3542] removed system configuration from code

--- plugin/config.go
+++ plugin/config.go
@@ -36,7 +36,6 @@ type session struct {
 }
 
 type pluginConfig struct {
-	plugin.SystemOptions `conf:"optional,name=System"`
 	// Timeout is the amount of time to wait for a server to respond when
 	// first connecting and on follow up operations in the session.
 	Timeout int `conf:"optional,range=1:30"`
