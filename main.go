ea1e7a161f564efcc13dcc5fc20d08b85b2819d0
Continuous Integration <ci@zabbix.com>
2024-08-19 13:21:25 +0300

    .......... [ZBXNEXT-826] updated release version for 7.0.3

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ the license. There is NO WARRANTY, to the extent permitted by law.`
 
 //nolint:gochecknoglobals,revive // required ALL_CAPS by build scripts
 var (
-	PLUGIN_VERSION_RC    = "rc1"
+	PLUGIN_VERSION_RC    = ""
 	PLUGIN_VERSION_MAJOR = 7
 	PLUGIN_VERSION_MINOR = 0
 	PLUGIN_VERSION_PATCH = 3
