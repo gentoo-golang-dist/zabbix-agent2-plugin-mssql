b758cd561e9471efa30f13e20272adae340a6437
Eriks Sneiders <eriks.sneiders@zabbix.com>
2024-11-04 13:25:36 +0200

    ...G...... [ZBX-25443] downgraded go version to 1.21

--- plugin/dbconn/dbconn_test.go
+++ plugin/dbconn/dbconn_test.go
@@ -144,6 +144,7 @@ func TestConnCollection_Init(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
@@ -944,6 +945,7 @@ func Test_newConnConfig(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
