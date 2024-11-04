8b9c67586d00a3ba92d8d216374eae40b6c40625
Eriks Sneiders <eriks.sneiders@zabbix.com>
2024-11-04 13:25:36 +0200

    ...G...... [ZBX-25443] downgraded go version to 1.21

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module golang.zabbix.com/plugin/mssql
 
-go 1.22
+go 1.21
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
--- plugin/dbconn/dbconn_test.go
+++ plugin/dbconn/dbconn_test.go
@@ -138,6 +138,7 @@ func TestConnCollection_Init(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
@@ -959,6 +960,7 @@ func Test_newConnConfig(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
