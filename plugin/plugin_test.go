fa928a3fbdd6d926d97d30b75a175b35d4b1f27a
Leonards Survilo <leonards.survilo@zabbix.com>
2025-05-26 14:25:27 +0300

    ...G...... [DEV-4298] updated Golang version to 1.23 & updated libs by DEV-4311

--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -90,9 +90,8 @@ func Test_mssqlPlugin_Start(t *testing.T) {
 
 			if diff := cmp.Diff(
 				tt.wantCons, p.conns,
-				cmp.AllowUnexported(
-					dbconn.ConnCollection{}, sync.Mutex{}, mssqlPlugin{},
-				),
+				cmp.AllowUnexported(dbconn.ConnCollection{}, sync.Mutex{}, mssqlPlugin{}),
+				cmpopts.IgnoreFields(dbconn.ConnCollection{}, "mu"),
 				cmpopts.IgnoreTypes(mssqlPlugin{}),
 			); diff != "" {
 				t.Fatalf("mssqlPlugin.Start() = %s", diff)
@@ -121,7 +120,6 @@ func Test_mssqlPlugin_Stop(t *testing.T) {
 		{"+valid", fields{conns: &dbconn.ConnCollection{}}},
 	}
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
@@ -297,7 +295,6 @@ func Test_mssqlPlugin_Export(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
@@ -338,7 +335,6 @@ func Test_mssqlPlugin_registerMetrics(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
