abe09564a51e2dbace6cb1d9208de486781a20cc
Eriks Sneiders <eriks.sneiders@zabbix.com>
2025-07-09 15:35:05 +0300

    ...G...... [ZBX-25834] fixed docker test environment

--- docker/setup/setup.sh
+++ docker/setup/setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 if [ "$MSSQL_VERSION" = "2022" ]; then
-    /opt/mssql-tools/bin/sqlcmd \
+    /opt/mssql-tools18/bin/sqlcmd \
         -U sa \
+        -C \
         -S "$MSSQL_URL" \
         -P "$MSSQL_SA_PASSWORD" \
         -i /root/setup-2022.sql
 else
-    /opt/mssql-tools/bin/sqlcmd \
+    /opt/mssql-tools18/bin/sqlcmd \
         -U sa \
+        -C \
         -S "$MSSQL_URL" \
         -P "$MSSQL_SA_PASSWORD" \
         -i /root/setup-2019-and-below.sql
