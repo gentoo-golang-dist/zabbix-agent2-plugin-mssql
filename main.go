82587ae9ed4f5a3abbd1daa465aec9b9a2eb1780
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2025-10-31 09:55:02 +0200

    .......... [ZBXNEXT-826] updated release version to 7.2.15rc1

--- main.go
+++ main.go
@@ -32,10 +32,10 @@ the license. There is NO WARRANTY, to the extent permitted by law.`
 
 //nolint:gochecknoglobals,revive // required ALL_CAPS by build scripts
 var (
-	PLUGIN_VERSION_RC    = ""
+	PLUGIN_VERSION_RC    = "rc1"
 	PLUGIN_VERSION_MAJOR = 7
 	PLUGIN_VERSION_MINOR = 2
-	PLUGIN_VERSION_PATCH = 14
+	PLUGIN_VERSION_PATCH = 15
 )
 
 func main() {
