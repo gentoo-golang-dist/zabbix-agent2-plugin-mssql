6f5472008d4bedfcebf87be1e1c01b4db4c114d0
Eriks Sneiders <eriks.sneiders@zabbix.com>
2025-11-19 16:23:44 +0200

    ...G...... [DEV-4581] fixed lint issues
    (cherry picked from commit b6114a198ea8a1fcf7264add40f25d5141d02771)

--- main.go
+++ main.go
@@ -79,9 +79,9 @@ func main() {
 
 func exitWithError(err error) {
 	fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-	os.Exit(1)
+	os.Exit(1) //nolint:revive // called in main
 }
 
 func exitGracefully() {
-	os.Exit(0)
+	os.Exit(0) //nolint:revive // called in main
 }
