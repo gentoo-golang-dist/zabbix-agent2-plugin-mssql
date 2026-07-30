cb9bfa7ca11af6f97413f3bc18b07192a5f91cb7
Natalja Romancaka <natalja.romancaka@zabbix.com>
2026-07-30 16:28:03 +0300

    .......... [ZBXNEXT-826] updated release version to 7.4.14rc1

--- main.go
+++ main.go
@@ -32,10 +32,10 @@ the license. There is NO WARRANTY, to the extent permitted by law.`
 
 //nolint:gochecknoglobals,revive,nolintlint // required ALL_CAPS by build scripts
 var (
-	PLUGIN_VERSION_RC    = ""
+	PLUGIN_VERSION_RC    = "rc1"
 	PLUGIN_VERSION_MAJOR = 7
 	PLUGIN_VERSION_MINOR = 4
-	PLUGIN_VERSION_PATCH = 13
+	PLUGIN_VERSION_PATCH = 14
 )
 
 func main() {
